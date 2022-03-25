80be67831df5e4abb9a608b4fed69acb560a06f4
Akumry <61588577+A874665@users.noreply.github.com>
2022-03-25 09:51:36 +0530

    Update newjava.java

--- newjava.java
+++ newjava.java
@@ -1,5 +1,5 @@
 class Add{
-  public static void main(String args[]{
+  public static void main(String args[]){
     System.out.printl("Hello this ios file for git");
   }
                           }
